abc5187c48df59764f3a053a1b160241d882b825
rainyang3 <77300717+rainyang3@users.noreply.github.com>
2021-05-19 09:28:27 +0800

    ss

--- body/script.js
+++ body/script.js
@@ -60,17 +60,17 @@ var cursor = {
         });
   
   
-        document.addEventListener('mousemove', function(e) {
-            // Show the cursor
-            self.cursorVisible = true;
-            self.toggleCursorVisibility();
+        // document.addEventListener('mousemove', function(e) {
+        //     // Show the cursor
+        //     self.cursorVisible = true;
+        //     self.toggleCursorVisibility();
 
-            // Position the dot
-            self.endX = e.pageX;
-            self.endY = e.pageY;
-            self.$dot.style.top = self.endY + 'px';
-            self.$dot.style.left = self.endX + 'px';
-        });
+        //     // Position the dot
+        //     self.endX = e.pageX;
+        //     self.endY = e.pageY;
+        //     self.$dot.style.top = self.endY + 'px';
+        //     self.$dot.style.left = self.endX + 'px';
+        // });
         
         // Hide/show cursor
         document.addEventListener('mouseenter', function(e) {
